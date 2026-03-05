80e6c95287d3e6adda65dc6b20c401b1dbcb86a1
Lukim99 <casting.acc001@gmail.com>
2026-03-06 00:27:48 +0900

    fix server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ server.all('/',(req,res)=>{
   res.send("카카오톡 봇 온라인");
 })
 function keepAlive() {
-  server.listen(3000, ()=>{ console.log("서버 준비 완료!") })
+  server.listen(8080, '0.0.0.0', () => {
+    console.log(`서버가 8080번 포트에서 돌아가고 있습니다 (0.0.0.0)`);
+  });
 }
 module.exports = keepAlive;

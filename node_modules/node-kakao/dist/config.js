fb303016a58fb0ecc19466e6a69f9e4e451126a5
Lukim99 <casting.acc001@gmail.com>
2026-04-14 03:43:57 +0900

    fix version

--- node_modules/node-kakao/dist/config.js
+++ node_modules/node-kakao/dist/config.js
@@ -22,7 +22,7 @@
         // eslint-disable-next-line max-len
         locoPEMPublicKey: `-----BEGIN PUBLIC KEY-----\nMIIBIDANBgkqhkiG9w0BAQEFAAOCAQ0AMIIBCAKCAQEArFhojUWXqu7GRj8GWNIgX5J6w23jbW3spYzLvQqLSKct6EVD6Ut9dfXCA/wCE/9FfPeJBEhqsY5JxYUEHVvz+2m7+cjDCxbQThSG5z1hDSggLxA30QRBF2/gKDo6um9Ng0q4QDO+3+mqVw1cVox0Xt++R4UdNT2BkVG+vp0T2c5e1QdeKvYnHYImPbeocGY+SHRcMWeZPfUrk0bLbnw6O/KDei5LOVk435LEsKHNtj7u4fswCVds4IFtgjjBrtrvhk4CitOcRrVVyeuODIuXy7g3dca1ZLPLxhb6fT25UtKd+8/jFTIMh4n/ul2u6pi7ny+WlEPPeBshwy4iPQ63PQIBAw==\n-----END PUBLIC KEY-----`,
         agent: 'android',
-        version: '10.2.0',
+        version: '11.0.0',
         osVersion: '7.1.2',
         // 0 == wired(WIFI), 3 == cellular
         netType: 0,
